780ab47e85f571793439576efa54bef9cef42531
Evan Nemerson <evan@nemerson.com>
2024-09-16 14:39:42 -0400

    test: make -Wattribute-warning a warning in unavailable test
    Basically, HEDLEY_UNAVAILABLE will use the warning attribute to get the
    compiler to emit a warning when the symbol is used, which is exactly
    what we want. However, recent versions of clang will turn that warning
    into an error if -Werror is specified. This is a great feature, but
    it causes our test to fail when we don't want it to.

--- test/unavailable.c
+++ test/unavailable.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+#if HEDLEY_GCC_HAS_WARNING("-Wattribute-warning",9,0,0)
+#  pragma clang diagnostic warning "-Wattribute-warning"
+#endif
+
 HEDLEY_UNAVAILABLE(3.14)
 static void
 test_unavailable(void) { }
