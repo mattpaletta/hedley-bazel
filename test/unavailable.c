94fd032c5938fe42fb0b1507b2ee471115283144
Evan Nemerson <evan@nemerson.com>
2024-09-17 08:19:06 -0400

    ci: add GCC CI builds

--- test/unavailable.c
+++ test/unavailable.c
@@ -1,7 +1,9 @@
 #include "common.h"
 
-#if HEDLEY_GCC_HAS_WARNING("-Wattribute-warning",9,0,0)
+#if HEDLEY_HAS_WARNING("-Wattribute-warning")
 #  pragma clang diagnostic warning "-Wattribute-warning"
+#elif HEDLEY_GCC_VERSION_CHECK(9,0,0)
+#  pragma GCC diagnostic warning "-Wattribute-warning"
 #endif
 
 HEDLEY_UNAVAILABLE(3.14)
